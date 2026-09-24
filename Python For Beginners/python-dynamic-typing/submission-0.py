df523b80f825a59b88b1fc9aa9a8ed489684351b
Daniel-Coder-BB <dannyluango48@gmail.com>
2026-09-24 20:04:51 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 4
+print(type(variable))
+variable = 5.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "str"
+print(type(variable))
+variable = [0,5]
+print(type(variable))
